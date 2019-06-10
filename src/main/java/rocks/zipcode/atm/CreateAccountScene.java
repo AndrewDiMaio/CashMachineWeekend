f3d946f0435ea7bb167f27565a6e68f1fec98458
Connor Jones <ConnorLeeJones@gmail.com>
2019-06-10 01:32:14 -0400

    Finally working

--- src/main/java/rocks/zipcode/atm/CreateAccountScene.java
+++ src/main/java/rocks/zipcode/atm/CreateAccountScene.java
@@ -3,25 +3,24 @@ package rocks.zipcode.atm;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import rocks.zipcode.atm.bank.*;
-
-import java.util.InputMismatchException;
-import java.util.Map;
+import javafx.stage.Stage;
 
 public class Scene2 extends SceneSetter{
     GridPane grid = new GridPane();
-    public CashMachine cashMachine = new CashMachine(new Bank());
 
 
-    Bank thisBank = new Bank();
+
+    Stage newStage;
+
+    //Bank thisBank;
     AccountScene loginSuccess;
     Scene thisScene;
 
@@ -41,15 +40,21 @@ public class Scene2 extends SceneSetter{
     TextField premiumTextField = new TextField();
 
 
+    Button exitButton = new Button("Exit");
 
 
+    //public CashMachine thisCashMachine;
 
-    public Scene2(){
 
+    public Scene2(CashMachine thisCashMachine){
+        //this.thisCashMachine = thisCashMachine;
+        //thisBank = thisCashMachine.getBank();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
+
+
         this.thisScene = (new Scene(grid, 500, 500));
 
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
@@ -72,6 +77,17 @@ public class Scene2 extends SceneSetter{
         hbBtn.getChildren().addAll(createButton);
         grid.add(hbBtn, 1, 10);
 
+        HBox exitBtn = new HBox(10);
+        exitBtn.setAlignment(Pos.BOTTOM_LEFT);
+        exitBtn.getChildren().addAll(exitButton);
+        grid.add(exitButton, 5, 10);
+
+
+        exitButton.setOnAction(e -> {
+            newStage.close();
+        });
+
+
 
         grid.add(actiontarget, 1, 6);
         createButton.setOnAction(e -> {
@@ -89,12 +105,15 @@ public class Scene2 extends SceneSetter{
             email = emailTextField.getText();
             premium = premiumField.getText();
 
-
+            if (name.equals("")){
+                name = "Anonymous";
+                email = "n/a";
+            }
 
 
             while (true) {
                 idInt = (int) (Math.random() * 10000);
-                if (!thisBank.accounts.containsKey(idInt)) {
+                if (!thisCashMachine.getBank().accounts.containsKey(idInt)) {
                     break;
                 }
             }
@@ -107,19 +126,38 @@ public class Scene2 extends SceneSetter{
                 premiumBoul = false;
             }
 
-            actiontarget.setText("Welcome, " + name + "! Your account number is " + id + ".");
-            thisBank.addAccount(id, name, email, premiumBoul);
-            System.out.println(thisBank.getAccounts());
-            this.cashMachine = new CashMachine(thisBank);
-            cashMachine.login(id);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Welcome to CTM");
+            alert.setHeaderText(null);
+            alert.setContentText("Welcome, " + name + "! Thank you for choosing CTM. Your account number is " + id + ".");
+
+            alert.showAndWait();
+            newStage.close();
+
+
+            thisCashMachine.getBank().addAccount(id, name, email, premiumBoul);
+            System.out.println(thisCashMachine.getBank().getAccounts());
+            System.out.println(thisCashMachine.getBank().getAllAccounts());
+
+            thisCashMachine.login(id);
+
+            SceneSetter callScene = new SceneSetter(thisCashMachine);
+            callScene.displayMainScene();
         });
 
     }
 
 
-    public Scene getScene2() {
-        return thisScene;
+    public void getCreateScene(){
+        newStage = new Stage();
+        newStage.setTitle("Account ");
+        newStage.setScene(thisScene);
+        newStage.show();
+
     }
+
+
 
     public void buttonLogic(){
 
